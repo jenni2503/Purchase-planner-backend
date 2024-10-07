7db462bd7a59fff79a25ba6056101067ce0b8b8a
jenni2503 <june-jenny@hotmail.com>
2024-10-07 21:48:47 +0200

    add published field

--- src/models/item.ts
+++ src/models/item.ts
@@ -1,26 +1,30 @@
-import mongoose from 'mongoose';
-import { Schema } from 'mongoose';
+import mongoose from "mongoose";
+import { Schema } from "mongoose";
 
 const itemSchema = new Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    store: {
-        type: String,
-    },
-    person: {
-        type: String,
-        required: true
-    },
-    price: {
-        type: String,
-        required: true
-    },
-    imageURL: {
-        type: String
-    },
-})
+  title: {
+    type: String,
+    required: true,
+  },
+  store: {
+    type: String,
+  },
+  person: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: String,
+    required: true,
+  },
+  imageURL: {
+    type: String,
+  },
+  published: {
+    type: Boolean,
+    default: false,
+  },
+});
 
-const Item = mongoose.model('Item', itemSchema);
+const Item = mongoose.model("Item", itemSchema);
 export default Item;
